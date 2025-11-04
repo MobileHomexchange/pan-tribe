31ee1040cba6233943c995078ede30f7d67ea783
MobileHomexchange <INFO@FAITHMHS.COM>
2025-11-04 13:47:32 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-// src/main.tsx
+// src/main.tsx - CORRECT IMPORT
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css"; // must include Tailwind entry + your tokens
+import App from "./App"; // ← Make sure this says "App" not "Ago"
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
